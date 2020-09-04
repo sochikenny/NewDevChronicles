59f592f54b9e69b3c9ae080afb33203a0471891b
Ikenna Nwajagu <Ikenna.nwajagu@gmail.com>
2020-09-03 20:53:31 -0400

    added a put and delete route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const path = require('path')
+const methodOverride = require('method-override')
 
 //Load Config
 dotenv.config({ path: './config/config.env'})
@@ -19,6 +20,16 @@ const app = express()
 //Body parser
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
+
+// Method Override
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 
 //logging
 if (process.env.NODE_ENV === 'development'){
